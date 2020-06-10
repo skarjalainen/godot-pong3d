59aaffad9dbcf2a9a5e7b93da07c4a994a0edb2a
Sami Karjalainen <ext-Sami.Karjalainen@opuscapita.com>
2020-06-10 08:27:36 +0300

    Change name

--- src/UI/HUD.gd
+++ src/UI/HUD.gd
@@ -1,10 +1,16 @@
 extends MarginContainer
 
+
+onready var Player1Name = get_node("ScoreContainer/Player1/DataContainer/Name")
+onready var Player2Name = get_node("ScoreContainer/Player2/DataContainer/Name")
 onready var Player1Score = get_node("ScoreContainer/Player1/DataContainer/Score")
 onready var Player2Score = get_node("ScoreContainer/Player2/DataContainer/Score")
 
+
 func _ready():
 	Global.connect("score_changed", self, "_on_Global_score_changed")
+	Player1Name.text = Global.playerData[0].name
+	Player2Name.text = Global.playerData[1].name
 
 
 func _on_Global_score_changed():
--- src/UI/MainMenu.gd
+++ src/UI/MainMenu.gd
@@ -10,11 +10,16 @@ func _ready():
 	pass
 
 
+func savePlayerName():
+	Global.playerData[0].name = NameInput.text
+
 func _on_SinglePlayerButton_pressed():
+	savePlayerName()
 	Scene.goto_scene("res://src/Levels/Level1.tscn")
 
 
 func _on_MultiPlayerButton_pressed():
+	savePlayerName()
 	Scene.goto_scene("res://src/UI/NetworkMenu.tscn")
 
 
